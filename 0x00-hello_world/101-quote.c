7c84e5a76b9c58a5d6bd8f7180fc2806149c9bbc
reeper <acebany080@gmail.com>
2023-03-16 05:25:37 -0700

    Complex Code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Start of the entry point
- * Description - Complex C program
- * Return:1
+ * main - Prints a text
+ * Description - Complex Code
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
