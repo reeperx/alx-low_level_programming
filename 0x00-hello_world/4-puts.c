036f1731339f03b186b2e0976e175add553dfbc4
reeper <acebany080@gmail.com>
2023-03-16 04:46:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints out text
+ * Description - Simple C program
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0)
+}
