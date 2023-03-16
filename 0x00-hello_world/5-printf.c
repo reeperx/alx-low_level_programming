7e4969951614a7b661502c093cc89671e97cd7a2
reeper <acebany080@gmail.com>
2023-03-16 04:58:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Print sample text
+ * Descrption - simple c program
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
