5e32b8f70a0d38ac6c3293881e8a923fe1c3bc9a
reeperx <acebany080@gmail.com>
2023-03-21 11:59:28 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - Prints out alphabets
- * Return: Void
- */
+ * Main - prints out alphabets
+ * Description - Alphabet program
+ * Return: 0 Always
+ **/
 
 void print_alphabet(void)
 {
 	char b = 'a';
+
 	while (b <= 'z')
 	{
 		_putchar(b);
