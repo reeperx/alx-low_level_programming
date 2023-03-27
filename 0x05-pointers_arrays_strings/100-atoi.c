bc51ac0c191e8240a68c60195dbc99cfed03078f
reeperx <acebany080@gmail.com>
2023-03-27 00:54:16 -0700

    arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - converts a string to an integer
+ * int _atoi() - converts a string to an integer
  * @s: string to be converted
  * Return: the int converted from the string
  */
