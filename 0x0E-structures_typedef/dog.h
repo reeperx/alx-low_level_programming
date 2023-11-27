38b9c5f9e2add7b65228840232d67e81e8aa980b
reeperx <acebany080@gmail.com>
2023-11-27 04:41:27 -0800

    changes to dog file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,14 @@
 #ifndef DOG
 #define DOG
-
-
-   /**
-    * struct dog - dog class
-    *
-    * @name: Name of the dog
-    *
-    * @age: Age of the dog
-    *
-    * @owner: Owner of the dog
-    */
-
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
 
 	struct dog
 {
@@ -20,18 +17,12 @@
 	char *owner;
 };
 
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-
 typedef struct dog dog_t;
 
-
 dog_t *new_dog(char *name, float age, char *owner);
-
-
 void free_dog(dog_t *d);
-
 
 #endif
