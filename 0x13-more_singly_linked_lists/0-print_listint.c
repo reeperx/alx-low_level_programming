116a8c44cfc7e3a7b6f8cdaeeee631f1923a169d
reeperx <acebany080@gmail.com>
2023-12-12 15:13:21 -0800

    new

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,15 @@
  *
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
 	while (h)
 	{
-	printf("%d\n", h->n);
-	num++;
-	h = h->next;
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
 	}
 
 	return (num);
